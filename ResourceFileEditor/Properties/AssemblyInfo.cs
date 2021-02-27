c855e76e61a734be77a7e0be0acd4232b4a64db8
MadDeCoDeR <gkwin8ac@hotmail.gr>
2021-02-27 12:57:55 +0200

    Update configurations

--- ResourceFileEditor/Properties/AssemblyInfo.cs
+++ ResourceFileEditor/Properties/AssemblyInfo.cs
@@ -28,11 +28,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ResourceFileEditor")]
+[assembly: AssemblyTitle("BFG Resource File Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ResourceFileEditor")]
+[assembly: AssemblyProduct("BFGResourceFileManager")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
